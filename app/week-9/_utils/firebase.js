0b3888874283a549088352e6f94516e0a3976cec
Naz Zaamout <166882349+nazzaamout@users.noreply.github.com>
2024-11-12 18:16:23 -0700

    page.js-week9

--- app/week-9/_utils/firebase.js
+++ app/week-9/_utils/firebase.js
@@ -2,18 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCXrSewFOMsnmJhcivmRT92XGNPaMN96xU",
+  authDomain: "cprg306-assignments-75200.firebaseapp.com",
+  projectId: "cprg306-assignments-75200",
+  storageBucket: "cprg306-assignments-75200.firebasestorage.app",
+  messagingSenderId: "1018642296457",
+  appId: "1:1018642296457:web:4cda37799f3de827e8e058",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,8 +1,57 @@
 // Import the useUserAuth hook
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
 
-// Use the useUserAuth hook to get the user object and the login and logout functions
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+export default function Page() {
+  // Use the useUserAuth hook to get the user object and the login and logout functions
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const login = async () => {
+    await gitHubSignIn();
+  };
+
+  const logout = async () => {
+    await firebaseSignOut();
+  };
+
+  return (
+    <main>
+      {user && (
+        <div>
+          <p>
+            Welcome, {user.displayName} ({user.email})
+          </p>
+
+          <div>
+            <ul>
+              <li className="flex no-underline hover:underline">
+                <Link href="/week-9/shopping-list">
+                  <a>Go To Shopping List</a>
+                </Link>
+              </li>
+
+              <li className="flex no-underline hover:underline">
+                <Link href="/week-9/profile-page">
+                  <a>Go To Profile Page</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {user ? (
+        <div className="flex no-underline hover:underline text-black">
+          <button onClick={logout}>Sign Out</button>
+        </div>
+      ) : (
+        <div className="flex no-underline hover:underline text-black">
+          <button onClick={login}>Sign In</button>
+        </div>
+      )}
+    </main>
+  );
+}
 
 // Sign in to Firebase with GitHub authentication
 await gitHubSignIn();
