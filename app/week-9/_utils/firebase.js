138ed4534762cf765df766f609f8ad18462717cb
Naz Zaamout <166882349+nazzaamout@users.noreply.github.com>
2024-11-12 18:40:36 -0700

    updated firebase.js

--- app/week-9/_utils/firebase.js
+++ app/week-9/_utils/firebase.js
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXrSewFOMsnmJhcivmRT92XGNPaMN96xU",
-  authDomain: "cprg306-assignments-75200.firebaseapp.com",
-  projectId: "cprg306-assignments-75200",
-  storageBucket: "cprg306-assignments-75200.firebasestorage.app",
-  messagingSenderId: "1018642296457",
-  appId: "1:1018642296457:web:4cda37799f3de827e8e058",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
