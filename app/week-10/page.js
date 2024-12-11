2e3110f8e2eb9bb230a8f7b5651d6f6f04365ce3
Naz Zaamout <166882349+nazzaamout@users.noreply.github.com>
2024-12-10 19:37:59 -0700

    c

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -54,30 +54,3 @@ export default function Page() {
   );
 }
 
-/**
- * "use client"
-
-import Link from 'next/link';
-import React from 'react';
-import { useUserAuth } from './_utils/auth-context';
-export default function Page() {
-
-    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth()
-
-    return (
-        <main className='p-5'>
-            {
-                !user ? <>
-
-                    <button onClick={gitHubSignIn}>Sign in to view options</button>
-
-                </>
-                    : <>
-                        <Link className='block mb-10' href={"/week-10/shopping-list"}>Go to shopping list</Link>
-                        <button onClick={firebaseSignOut}>Sign Out</button>
-                    </>
-            }
-        </main>
-    )
-}
- */
--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -49,14 +49,6 @@ export default function Page() {
     loadItems();
   }, [user]);
 
-  if (!user) {
-    return (
-      <div>
-        <h2>Sign in to view this page.</h2>
-      </div>
-    );
-  }
-
   return (
     <main className="flex bg-center bg-black min-h-screen text-gray-200 p-5">
       <div className="flex w-full">
@@ -76,66 +68,3 @@ export default function Page() {
     </main>
   );
 }
-
-/**
- * "use client"
-
-import React, { useEffect, useState } from 'react';
-import ItemList from './item-list';
-import NewItem from './new-item';
-import MealIdeas from './meal-ideas';
-import { addItem, getItems } from '../_services/shopping-list-service';
-import { useUserAuth } from '../_utils/auth-context';
-
-export default function Page() {
-
-    const { user } = useUserAuth()
-
-    const [items, setItems] = useState([])
-    const [selectedItemName, setSelectedItemName] = useState('')
-
-    const handleAddItem = (item) => {
-        const id = addItem(user.uid, item)
-
-        const newItem = {
-            ...item,
-            id
-        }
-
-        setItems([...items, newItem])
-    }
-
-    const handleItemSelect = itemName => {
-        setSelectedItemName(itemName)
-    }
-
-    async function loadItems() {
-        const items = await getItems(user.uid)
-        setItems(items)
-    }
-
-    useEffect(() => {
-        if (!user) return
-        loadItems()
-    }, [user])
-
-    return (
-        <main className='p-5'>
-            {
-                user === null ? <p>You need to sign in to view this page</p> : (
-                    <>
-                        <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-                        <div className='flex'>
-                            <div className='w-1/2'>
-                                <NewItem onAddItem={handleAddItem} />
-                                <ItemList items={items} onItemSelect={handleItemSelect} />
-                            </div>
-                            <MealIdeas ingredient={selectedItemName} />
-                        </div>
-                    </>
-                )
-            }
-        </main>
-    )
-}
- */
